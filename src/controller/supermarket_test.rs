cc604238cdfd9ce3bd6978e3cc4e77ff354af48d
juankhusuma <66712975+juankhusuma@users.noreply.github.com>
2024-05-26 00:39:40 +0700

    [GREEN] Implemented controller updates

--- src/controller/supermarket_test.rs
+++ src/controller/supermarket_test.rs
@@ -1,12 +1,23 @@
 #[cfg(test)]
 mod tests {
-    use crate::{controller::supermarket::find_all, repository::lib::setup};
-    use rocket::{tokio, State};
+    use crate::{
+        controller::supermarket::{find_all, find_by_id},
+        guard::auth_guard::AuthGuard,
+        repository::lib::setup,
+    };
+    use rocket::{request::FromRequest, tokio, Request, State};
 
     #[tokio::test]
     async fn test_find_all_supermarkets() {
         let pool = setup().await;
         let response = find_all(State::from(&pool)).await;
+        assert_eq!(response.is_err(), false);
+    }
+
+    #[tokio::test]
+    async fn test_get_supermarket_by_id() {
+        let pool = setup().await;
+        let response = find_by_id(State::from(&pool), 1).await;
         assert_eq!(response.is_err(), false);
     }
 }
